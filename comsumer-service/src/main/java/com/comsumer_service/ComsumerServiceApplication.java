f6d6210a69e28f4b689125bf0d3c0e59ca584d72
namtv1 <namtv1@nal.vn>
2024-09-19 13:48:40 +0700

    init

---
+++ comsumer-service/src/main/java/com/comsumer_service/ComsumerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.comsumer_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComsumerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComsumerServiceApplication.class, args);
+	}
+
+}
---
+++ comsumer-service/src/test/java/com/comsumer_service/ComsumerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comsumer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComsumerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
